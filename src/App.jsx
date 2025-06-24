8b75deeef2f6e515339d2a35e29adc00e44d79c0
Alameen <Adekualameem@gmail.com>
2025-06-24 14:42:28 -0500

    Disable button logic added

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [activeCard, setActiveCard] = useState(0);
   const handleNext = () => {
     let nextCard;
-    if (activeCard == questions.length - 1) setActiveCard(0);
+    if (activeCard == questions.length - 1) return;
     else {
       nextCard = activeCard + 1;
       setActiveCard(nextCard);
@@ -14,7 +14,7 @@ function App() {
   };
   const handleBack = () => {
     let prevCard;
-    if (activeCard == 0) setActiveCard(questions.length - 1);
+    if (activeCard == 0) return;
     else {
       prevCard = activeCard - 1;
       setActiveCard(prevCard);
@@ -27,17 +27,25 @@ function App() {
       <p>Number of cards: {questions.length}</p>
 
       <QuizCard
-        key = {activeCard}
+        key={activeCard}
         question={questions[activeCard].question}
         answer={questions[activeCard].answer}
         category={questions[activeCard].category}
       ></QuizCard>
 
       <div className="button-container">
-        <button className="forward-button" onClick={handleBack}>
+        <button
+          className="back-button"
+          onClick={handleBack}
+          disabled={activeCard == 0? true: false}
+        >
           Back
         </button>
-        <button className="back-button" onClick={handleNext}>
+        <button
+          className="forward-button"
+          onClick={handleNext}
+          disabled={activeCard == questions.length-1? true: false}
+        >
           Next
         </button>
       </div>
