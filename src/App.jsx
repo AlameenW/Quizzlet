c78cbf30c4b78057420267d6dc179007f6780a5b
Alameen <Adekualameem@gmail.com>
2025-06-24 23:15:46 -0500

    User guess logic implemented

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ import questions from "./questions";
 import QuizCard from "./components/QuizCard";
 function App() {
   const [activeCard, setActiveCard] = useState(0);
+  const [userAnswer, setUserAnswer] = useState("");
+  const [answerStyle, setanswerStyle] = useState('')
   const handleNext = () => {
     let nextCard;
+    setUserAnswer('');
+    setanswerStyle('');
     if (activeCard == questions.length - 1) return;
     else {
       nextCard = activeCard + 1;
@@ -14,11 +18,23 @@ function App() {
   };
   const handleBack = () => {
     let prevCard;
+    setUserAnswer('');
+    setanswerStyle('');
     if (activeCard == 0) return;
     else {
       prevCard = activeCard - 1;
       setActiveCard(prevCard);
     }
+  };
+  const normalize = (str) => {
+    const normalized = str.trim().toLowerCase().replace(/[^\w\s]/gi, '');
+    return normalized;
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (normalize(userAnswer) == normalize(questions[activeCard].answer.toLowerCase()))
+      setanswerStyle('correct');
+    else setanswerStyle('wrong');
   };
   return (
     <div>
@@ -33,18 +49,40 @@ function App() {
         category={questions[activeCard].category}
       ></QuizCard>
 
+      <div className="form-container">
+        <form>
+          <label htmlFor="uer-answer">Guess the answer: </label>
+          <input
+            className={answerStyle}
+            type="text"
+            id="user-answer"
+            name="user-answer"
+            value={userAnswer}
+            onChange={(e) => {
+              setUserAnswer(e.target.value);
+            }}
+            placeholder="Place your answer here"
+          />
+          <button
+            onClick={handleSubmit}
+          >
+            Submit Guess
+          </button>
+        </form>
+      </div>
+
       <div className="button-container">
         <button
           className="back-button"
           onClick={handleBack}
-          disabled={activeCard == 0? true: false}
+          disabled={activeCard == 0 ? true : false}
         >
           Back
         </button>
         <button
           className="forward-button"
           onClick={handleNext}
-          disabled={activeCard == questions.length-1? true: false}
+          disabled={activeCard == questions.length - 1 ? true : false}
         >
           Next
         </button>
